f9525a780abad9fbe7149158fe60d6e442a523df
Aadildar1 <116451473+Aadildar1@users.noreply.github.com>
2026-02-08 23:23:06 +0530

    Add login URL to output message
    Updated the output message to include a login URL.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! Maven build successful.");
+        System.out.println("Hello, Jenkins! Maven build successful.\nhttp://localhost:8080/login?from=%2FsetupWizard%2FsetupWizardFirstUser");
     }
 }
+
